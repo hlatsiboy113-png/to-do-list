c4f3b92772b58e5b529743cd30fd4160238829b1
Emily Maramani <emilyqm01@gmail.com>
2026-06-04 11:47:16 +0200

    Added priority levels to tasks

--- script.js
+++ script.js
@@ -20,10 +20,13 @@ function addTask() {
   const text = input.value.trim();
   if (text === "") return;
 
+  const priority = document.getElementById("priority").value;
+
   const task = {
     id: Date.now(),
     text,
-    completed: false
+    completed: false,
+    priority: priority
   };
 
   tasks.push(task);
@@ -46,13 +49,30 @@ function renderTasks() {
     const li = document.createElement("li");
 
     li.innerHTML = `
-      <span class="${task.completed ? "completed" : ""}" 
-            onclick="toggleTask(${task.id})">
-        ${task.text}
-      </span>
-      <button onclick="deleteTask(${task.id})">X</button>
-    `;
+  <span class="${task.completed ? "completed" : ""}" 
+        onclick="toggleTask(${task.id})">
+    ${task.text}
+  </span>
 
+  <small style="
+    margin-left:10px;
+    padding:2px 6px;
+    border-radius:5px;
+    font-size:10px;
+    background:${
+      task.priority === "high"
+        ? "#ff4d4d"
+        : task.priority === "medium"
+        ? "#ffa500"
+        : "#4caf50"
+    };
+    color:white;
+  ">
+    ${task.priority.toUpperCase()}
+  </small>
+
+  <button onclick="deleteTask(${task.id})">X</button>
+`;
     taskList.appendChild(li);
   });
 }
